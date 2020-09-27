c782ecc8f359b07dcada613c3ab72724c468c1b1
jmelo <jmeloa@gmail.com>
2020-09-27 09:38:26 -0300

    subida a heroku

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  resources :likes
+  resources :tweets   
+  devise_for :users, controllers:{registrations: 'registrations'}
+  
+  root 'home#index'  
+  resources :tweets do
+    resources :likes
+  end
+
+  
+
+    #devise_scope :user do
+    #  get "/home_privados/index" => "home_privados_controller"
+    #end
+    
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
